0a5045cb18a08337e47bcb7941ec18aa9e28cf34
Amanda Louzada <amanda.louzada@nubank.com.br>
2022-05-11 20:19:25 -0300

    Validando operação

--- src/models/conta.js
+++ src/models/conta.js
@@ -1,17 +1,30 @@
 
+const ENUM_OPERACAO = {
+    deposito: 'Depósito',
+    retirada: 'Retirada'
+}
 
 class Conta {
     constructor (){
         this.extrato = [];
     }
-   
+    
+    _validaOperacao(operacao){
+        if (ENUM_OPERACAO[operacao]){
+            return true;
+        }
+        return false;
+    }
+
     incluirOperacao({operacao,valor}){
-        this.extrato.push({
-            operacao: operacao, 
-            valor: valor,
-            data: new Date()
-    })
- }
+        if(this._validaOperacao(operacao)) {
+            this.extrato.push({
+                operacao: operacao, 
+                valor: valor,
+                data: new Date()
+            })
+        }
+    }
 
 }
 module.exports = new Conta();
