093163534eb08fcd7336dfc8f81d82d90a698923
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-05-11 21:05:13 -0300

    tests conta part 1

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,36 @@
-describe("Teste unitários", () => {
-  test("Deve ser 4", () => {
-    const soma = 2+2;
-    expect(soma).toBe(4);
+const conta = require("./models/conta");
+
+describe("Teste Conta", () => {
+  test("Deve ser inválida", () => {
+    const ehValida = conta._validaOperacao("invalida");
+    expect(ehValida).toBe(false);
+  });
+  test("Depósito deve ser um tipo de operação válida", () => {
+    const depositoEhValido = conta._validaOperacao("deposito");
+    const retiradaEhValido = conta._validaOperacao("retirada");
+    expect(depositoEhValido).toBe(true);
+    expect(retiradaEhValido).toBe(true);
+  });
+  test("Deve retornar o valor do saldo", () => {
+    conta.extrato.push(
+      {
+        operacao: "deposito",
+        valor: 100,
+      },
+      {
+        operacao: "retirada",
+        valor: 20,
+      }
+    );
+    const valorSaldo = conta.calcularSaldo();
+    expect(valorSaldo).toBe(80);
+  });
+  test("Deve retornar o valor do saldo 0", () => {
+    const valorSaldo = conta.calcularSaldo();
+    expect(valorSaldo).toBe(0);
+  });
+  test("Saldo não pode ser negativo", () => {
+    const valorSaldo = conta.calcularSaldo();
+    expect(valorSaldo).toBeGreaterThanOrEqual(0);
   });
 });
